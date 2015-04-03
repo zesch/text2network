e035200024551c180707f41dcd78f2f7f4c41df7
tobias.graeve@gmail.com <tobias.graeve@gmail.com@ee02c819-6725-ad69-4150-6c9bc07b1bd0>
2015-04-03 23:54:53 +0000

    Unit-Tests für Annotator hinzugefügt.

---
+++ Text2Network/src/test/java/de/tudarmstadt/ukp/experiments/tgraeve/text2network/SpotlightAnnotatorTest.java
@@ -0,0 +1,70 @@
+package de.tudarmstadt.ukp.experiments.tgraeve.text2network;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.junit.Assert.*;
+
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.NC;
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.VC;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpChunker;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.experiments.tgraeve.text2network.annotator.ChunkTagChanger;
+import de.tudarmstadt.ukp.experiments.tgraeve.text2network.annotator.ConceptAnnotator;
+import de.tudarmstadt.ukp.experiments.tgraeve.text2network.annotator.SpotlightAnnotator;
+import de.tudarmstadt.ukp.experiments.tgraeve.text2network.type.Concept;
+
+public class SpotlightAnnotatorTest {
+	
+	protected String text = "Germany was reunified in 1990.";
+	protected static double confidence = 0.2;
+	
+	@Test
+	public void testConceptAnnotationNC() throws UIMAException
+	{
+		JCas jcas = JCasFactory.createJCas();
+		jcas.setDocumentText(text);
+		jcas.setDocumentLanguage("en");
+		
+		//Segmenter
+		AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngine segEngine = createEngine(segmenter);
+		segEngine.process(jcas);
+		
+		//SpotlightAnnotator
+		AnalysisEngineDescription spotAnn = createEngineDescription(SpotlightAnnotator.class);
+		AnalysisEngine spotEngine = createEngine(spotAnn);
+		spotEngine.process(jcas);
+		
+		//Test
+		Concept result = JCasUtil.selectSingle(jcas, Concept.class);
+		assertEquals("Germany", result.getCoveredText());
+	}
+}
