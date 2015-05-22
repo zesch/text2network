d7602c8e2a333f728d3597f0ef28c1df8262a320
Torsten Zesch <torsten.zesch@gmail.com>
2015-05-22 10:21:55 +0200

    Removing input/output folders

--- Text2Network/src/main/java/de/tudarmstadt/ukp/experiments/tgraeve/text2network/Example.java
+++ Text2Network/src/main/java/de/tudarmstadt/ukp/experiments/tgraeve/text2network/Example.java
@@ -52,7 +52,7 @@ public class Example {
 				createReaderDescription(
 				         TextReader.class,
 				         TextReader.PARAM_SOURCE_LOCATION, "src/test/resources/text",
-				         TextReader.PARAM_PATTERNS, new String[] {"[+]*.txt"},
+				         TextReader.PARAM_PATTERNS, new String[] {"[+]simpsons.txt"},
 				         TextReader.PARAM_LANGUAGE, "en"
 				 ), 
 				createEngineDescription(
